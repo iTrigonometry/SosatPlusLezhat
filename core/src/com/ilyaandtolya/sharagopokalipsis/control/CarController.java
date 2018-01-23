f4db379b41af03d487cda5e6cebb173a7d264c1f
iTrigonometry <macdevzenit@gmail.com>
2018-01-23 17:11:06 +0300

    ono edet

--- core/src/com/ilyaandtolya/sharagopokalipsis/control/CarController.java
+++ core/src/com/ilyaandtolya/sharagopokalipsis/control/CarController.java
@@ -7,56 +7,56 @@ import com.badlogic.gdx.math.Polygon;
 import com.ilyaandtolya.sharagopokalipsis.view.GameScreen;
 
 public class CarController {
-    private float carSpeed, speedVelocity = 10f, speedMax = 10f;
-    private float rotationSpeed = 30f;
-
     private Polygon carBounds;
-    public CarController(Polygon carBounds) {
+
+    public CarController(Polygon carBounds){
         this.carBounds = carBounds;
     }
 
-
-    public void handle(){
-        //всё, что связано со скоростью
-        if (Gdx.input.isKeyPressed(Input.Keys.UP))
+    private float carSpeed, speedVelocity = 10f, speedMax = 10f;
+    private float rotationSpeed = 30f;
+    public void handle() {
+        //все что связано со скоростью
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
             carSpeed += speedVelocity * GameScreen.deltaCff;
-        else if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
+        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
             carSpeed -= speedVelocity * GameScreen.deltaCff;
-        else
+        } else {
             downSpeed();
-        carSpeed = sliceSpeed();
+        }
+            carSpeed = sliceSpeed();
         //
 
-        //всё, что связано с поворотом
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+
+        //все что связано с поворотом
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
             carBounds.rotate(rotationSpeed * carSpeed * GameScreen.deltaCff);
-        else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+        }else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
             carBounds.rotate(-rotationSpeed * carSpeed * GameScreen.deltaCff);
-        ///
+        }
 
-
-        carBounds.setPosition(carBounds.getX() + MathUtils.sinDeg(carBounds.getRotation() + 90) * carSpeed * GameScreen.deltaCff,
-                carBounds.getY() + MathUtils.cosDeg(carBounds.getRotation() + 90) * carSpeed * GameScreen.deltaCff);
+            carBounds.setPosition(carBounds.getX() + MathUtils.sinDeg(carBounds.getRotation() + 90) * carSpeed * GameScreen.deltaCff,
+                    carBounds.getY() + MathUtils.cosDeg(carBounds.getRotation() - 90) * carSpeed * GameScreen.deltaCff);
 
     }
 
-    private void downSpeed(){
-        if (carSpeed > speedVelocity * GameScreen.deltaCff)
-            carSpeed -= speedVelocity * GameScreen.deltaCff;
-        else if (carSpeed < -speedVelocity * GameScreen.deltaCff)
-            carSpeed += speedVelocity * GameScreen.deltaCff;
-        else
-            carSpeed = 0f;
+    private void downSpeed(){ //уменьшение скорости при не нажатых клавишах
+           if(carSpeed > speedVelocity * GameScreen.deltaCff){
+               carSpeed -= speedVelocity * GameScreen.deltaCff;
+           }else if (carSpeed < speedVelocity * GameScreen.deltaCff){
+               carSpeed += speedVelocity * GameScreen.deltaCff;
+           }else{
+               carSpeed =0f;
+           }
     }
 
-    private float sliceSpeed(){
-        if (carSpeed > speedMax)
-            return speedMax;
-        if (carSpeed < -speedMax)
-            return -speedMax;
-        return carSpeed;
+    private float sliceSpeed(){ //ограничение скорости
+      if (carSpeed > speedMax){
+          return speedMax;
+      }else if (carSpeed < - speedMax){
+          return -speedMax;
+      }
+
+      return carSpeed;
     }
-
-
-
 }
